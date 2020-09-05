9160f8c74bd3176f7dbb5d8f72d9bd1d932c0bc8
Johannes Martinez <mart4695@morris.umn.edu>
2020-09-05 15:25:06 -0500

    Get directory name from user
    We take in the directory name as an argument

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+# Get directory name given by user
+sourceDirectory=$1
+
+# Go to the given directory, if it exists
+cd "$sourceDirectory" || exit
+
+# cat name* outputs the content of all files that start with 'name'
+# The output of cat is piped to 
+cat secure* | awk '/.*Failed password for.*/ { print;}'
