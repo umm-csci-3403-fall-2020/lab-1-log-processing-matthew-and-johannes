3639c0dc63be0494e9891119ee3d96e9db2aef69
Johannes Martinez <mart4695@morris.umn.edu>
2020-09-06 07:24:02 -0500

    Edit * to ./* so file names don't have a chance to become commands

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -6,7 +6,7 @@ sourceDirectory=$1
 # Go to the given directory, if it exists
 cd "$sourceDirectory" || exit
 
-# cat * outputs the content of all files in the selected directory
+# cat ./* outputs the content of all files in the selected directory
 # The output of cat is piped to awk for filtering. By breaking down the text into small groups, and using backreferences, it is possible to extract desired content. Sed is used to remove double spaces in sd numbers.
-cat *| awk -F: 'match($0, /([A-Za-z0-9 ]+):([A-Za-z0-9 : \[ \] ]+) ([a-zA-Z0-9]+) ([a-zA-Z0-9]+) from([[0-9 \.]+) /, groups) {print groups[1]" "groups[4]groups[5]}' | sed 's/[ ]\+/ /g' >  failed_login_data.txt
+cat ./* | awk -F: 'match($0, /([A-Za-z0-9 ]+):([A-Za-z0-9 : \[ \] ]+) ([a-zA-Z0-9]+) ([a-zA-Z0-9]+) from([[0-9 \.]+) /, groups) {print groups[1]" "groups[4]groups[5]}' | sed 's/[ ]\+/ /g' > failed_login_data.txt
 
