6236345a776bb60338b51c6d056dca5dc20940ea
Johannes Martinez <mart4695@morris.umn.edu>
2020-09-06 07:09:27 -0500

    Add regex filter
    We use pipes to get the file output from cat and filter it with awk and regex. We remove white spaces with sed and write the results to a file.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -7,5 +7,6 @@ sourceDirectory=$1
 cd "$sourceDirectory" || exit
 
 # cat name* outputs the content of all files that start with 'name'
-# The output of cat is piped to awk for filtering
-cat secure* | awk '/.*Failed password for.*/ { print;}'
+# The output of cat is piped to awk for filtering. By breaking down the text into small groups, and using backreferences, it is possible to extract desired content. Sed is used to remove double spaces in sd numbers.
+cat *| awk -F: 'match($0, /([A-Za-z0-9 ]+):([A-Za-z0-9 : \[ \] ]+) ([a-zA-Z0-9]+) ([a-zA-Z0-9]+) from([[0-9 \.]+) /, groups) {print groups[1]" "groups[4]groups[5]}' | sed 's/[ ]\+/ /g' >  failed_login_data.txt
+
