73ddb05c2b616fefb03bdb3d731ae4f0edf50e74
miltort <spilm005@umn.edu>
2020-09-06 21:06:38 -0500

    Changed comments for clarity

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#cat takes all the failed_login_data files and cats them, then is handed to awk which should hunt down any usernames. Then it is all sorted and handed to uniq which counts how many instances of each name there is (in theory, I haven't tested it since it isn't completed)
+#cat takes all the failed_login_data files and cats them, then is handed to awk which should hunt down any usernames. Then it is all sorted and handed to uniq which counts how many instances of each name there is which then is given to another awk that puts it into the format we need. Then it makes a file that is used in the wrap_contents.
 cat "$1"/*/failed_login_data.txt | awk -F: 'match($0, /([A-Za-z]+) ([0-9 ]+)([A-Za-z0-9]+) ([0-9 \.]+)/, groups) {print groups[3]}' | sort | uniq -c | awk '/.*/ {print "data.addRow([\x27"$2"\x27, "$1"]);"}' > ./data/username_dist_mid.html
 
 ./bin/wrap_contents.sh ./data/username_dist_mid.html ./html_components/username_dist "$1"/username_dist.html
