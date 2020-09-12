20dee0d4b3226a0c1e8a6731f42bdf76f0ec4bd4
Johannes Martinez <mart4695@morris.umn.edu>
2020-09-12 16:17:17 -0500

    Add a space before regex group in order not to include trailing username numbers in IP address
    This issue caused us to count the 123 in username test123 as a part of an IP address

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -3,7 +3,7 @@
 # Open the failed_login_data.txt files in all subdirectoris of the directory given by the user
 # Use awk and regex to find the IP address numbers
 # Sort the output with sort
-cat "$1"/*/failed_login_data.txt | awk -F: 'match($0, /([0-9.]{3,})/, groups) {print groups[1]}' | sort |
+cat "$1"/*/failed_login_data.txt | awk -F: 'match($0, / ([0-9.]{3,})/, groups) {print groups[1]}' | sort |
 
 # Use join to join the IP addresses with the country codes in the etc/country_IP_map.txt file
 # The -o 2.2 option specifies that we want to only print the country codes,
