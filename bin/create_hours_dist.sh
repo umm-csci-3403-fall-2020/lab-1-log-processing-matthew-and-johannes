900d4091b2582b2dbbb50ae2e5fb05f99d327bf3
Johannes Martinez <mart4695@morris.umn.edu>
2020-09-12 13:31:44 -0500

    Add comments to create_hours_dist.sh and fix regex

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-cat "$1"/*/failed_login_data.txt | awk -F: 'match($0, /([A-Za-z]+) ([0-9]+) ([0-9]+)([A-Za-z0-9]+) ([0-9 \.]+)/, groups) {print groups[3]}' | sort | uniq -c | awk '/.*/ {print "data.addRow([\x27"$2"\x27, "$1"]);"}' > ./data/hours_dist_mid.html
+# Open the failed_login_data.txt files in all subdirectoris of the directory given by the user
+# Use awk to find the second column of numbers (which corresponds to the log hours)
+# Sort the output with sort
+cat "$1"/*/failed_login_data.txt | awk -F: 'match($0, / ([0-9]+) ([0-9]+)/, groups) {print groups[2]}' | sort |
 
+# Use uniq with count option (-c) to count the number of occurances of each clock hour
+# Use awk to print this data into the appropriate fields of the hours_dist_mid.html file
+uniq -c | awk '/.*/ {print "data.addRow([\x27"$2"\x27, "$1"]);"}' > ./data/hours_dist_mid.html
+
+# Wrap contents with a header and footer using the wrap_contents.sh script
 ./bin/wrap_contents.sh ./data/hours_dist_mid.html ./html_components/hours_dist "$1"/hours_dist.html
