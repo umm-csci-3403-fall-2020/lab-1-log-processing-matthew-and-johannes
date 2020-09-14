feab46b6f54bf96788b95455b9b45c7d6729f706
Johannes Martinez <mart4695@morris.umn.edu>
2020-09-13 21:39:21 -0500

    Preliminary commit for team debugging

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -2,19 +2,25 @@
 
 temp=$(mktemp -d)
 
-current= pwd
+current=$(pwd)
 
+mkdir -p "$temp"/data
 for var in "$@"
 do
-	tar --gunzip -xf "$var" | ./bin/process_client_logs.sh
+	# The directory we'll create to store the unzipped files
+	# We use awk and regex to remove unneeded chars
+	directoryName=$(echo "$var" | awk -F: 'match($0, /([a-z_]+)\/([a-z]+)/, groups) {print groups[2]}')
+	mkdir "$temp"/"$directoryName"
+	tar -C "$temp"/"$directoryName" -xf "$var" 
+	./bin/process_client_logs.sh "$temp"/"$directoryName"
 done
 
 ./bin/create_username_dist.sh "$temp"
 
-#./bin/create_hours_dist.sh "$temp"
+./bin/create_hours_dist.sh "$temp"
 
-#./bin/create_country_dist.sh "$temp"
+./bin/create_country_dist.sh "$temp"
 
-#./bin/assemble_report.sh "$temp"
+./bin/assemble_report.sh "$temp"
 
-#mv failed_login_summary.html "$current"
+mv "$temp"/failed_login_summary.html "$current"
