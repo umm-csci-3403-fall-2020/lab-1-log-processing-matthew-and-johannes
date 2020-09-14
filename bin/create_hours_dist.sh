bd35e72f032494999510919f972ad3f7fef7a47b
miltort <spilm005@umn.edu>
2020-09-13 22:05:06 -0500

    Changed the create helper functions to work better
    changed the helper functions to be less specific and place the mids
    in better locations

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -7,7 +7,7 @@ cat "$1"/*/failed_login_data.txt | awk -F: 'match($0, / ([0-9]+) ([0-9]+)/, grou
 
 # Use uniq with count option (-c) to count the number of occurances of each clock hour
 # Use awk to print this data into the appropriate fields of the hours_dist_mid.html file
-uniq -c | awk '/.*/ {print "data.addRow([\x27"$2"\x27, "$1"]);"}' > ./data/hours_dist_mid.html
+uniq -c | awk '/.*/ {print "data.addRow([\x27"$2"\x27, "$1"]);"}' > "$1"/hours_dist_mid.html
 
 # Wrap contents with a header and footer using the wrap_contents.sh script
-./bin/wrap_contents.sh ./data/hours_dist_mid.html ./html_components/hours_dist "$1"/hours_dist.html
+./bin/wrap_contents.sh "$1"/hours_dist_mid.html ./html_components/hours_dist "$1"/hours_dist.html
--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -4,6 +4,5 @@
 # Then it is all sorted and handed to uniq which counts how many instances of each name 
 # there are which then is given to another awk that puts it into the format we need. 
 # Then it makes a file that is used in the wrap_contents.
-cat "$1"/*/failed_login_data.txt | awk -F: 'match($0, /([A-Za-z]+) ([0-9 ]+)([A-Za-z0-9]+) ([0-9 \.]+)/, groups) {print groups[3]}' | 
-sort | uniq -c | awk '/.*/ {print "data.addRow([\x27"$2"\x27, "$1"]);"}' > ./data/username_dist_mid.html
-./bin/wrap_contents.sh ./data/username_dist_mid.html ./html_components/username_dist "$1"/username_dist.html
+cat "$1"/*/failed_login_data.txt | awk -F: 'match($0, /([A-Za-z]+) ([0-9 ]+)([A-Za-z0-9]+) ([0-9 \.]+)/, groups) {print groups[3]}' | sort | uniq -c | awk '/.*/ {print "data.addRow([\x27"$2"\x27, "$1"]);"}' > "$1"/username_dist_mid.html
+./bin/wrap_contents.sh "$1"/username_dist_mid.html ./html_components/username_dist "$1"/username_dist.html
